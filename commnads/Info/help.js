b8843d9499c030767c4a35480b180366f8fccdcb
Kaixin <48685463+GarudaID@users.noreply.github.com>
2022-01-17 09:25:44 +0700

    Create help.js

---
+++ commnads/Info/help.js
@@ -0,0 +1,9 @@
+const request = require('node-superfetch');
+const Color = "RANDOM";
+const Discord = require("discord.js");
+const disbut = require('discord-buttons');
+const { MessageActionRow, MessageButton } = require("discord-buttons");
+const { prefix, developerID, bot, support } = require("../../config.json")
+
+
+//  UNDER DEVELOPMEN, PLEASE COME BACK AGAIN LATER 
